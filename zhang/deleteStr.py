86c8780e204ff085293d432ea952fae3b162ce20
梁江浩 <l@liangjianghaodeMacBook-Pro.local>
2017-12-27 11:49:31 +0800

    delete

--- zhang/deleteStr.py
+++
@@ -1,9 +0,0 @@
-
-# -- coding: UTF-8 -- 
-import time
-import sys
-import os
-with open('zhang.txt', 'r') as f:  
-    data = f.readlines() 
-for zhanghao in data:
-	print zhanghao.strip()
--- zhang/twoType.py
+++
@@ -1,9 +0,0 @@
-url='http://www.btrabbit.cc/search/%s.html'%name
-print url	
-response=requests.get(url).content
-selector = html.fromstring(response)
-hrefs=selector.xpath('//div[@class="search-item detail-width"]')
-
-selector = html.fromstring(driver.page_source)
-# hrefs=selector.xpath('//div[@id="table_files_wrapper"]')
-hrefs=selector.xpath('//table/tbody/tr')
